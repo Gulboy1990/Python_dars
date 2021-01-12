edc91cdd75c948150b097926e8f2e9f4e164aab3
Gulboy <76957086+Gulboy1990@users.noreply.github.com>
2021-01-12 16:35:51 +0500

    Add files via upload

---
+++ 1_dars.py
@@ -0,0 +1,14 @@
+
+# Darvozaning eni 4metr, bo'yi esa"
+# darvoza yuzasi (s)ni hisoblash dasturini tuzing"
+# darvoza qirrasining uzunligi (p)ni hisoblash dasturini tuzing:
+
+e = 4
+b = 3
+
+s = e*b
+print('s=', s,'metr kvadrat') 
+
+p = 2 * (e + b)
+
+print('p=', p, 'metr')
---
+++ 2_dars.py
@@ -0,0 +1,19 @@
+
+# Ikkita qayiq turg'un suvda bir-biriga tomon 4 km/soat va 
+# 2km/soat tezlik bilan suzmoqda. Ular orasidagi masofa 24km bo'lsa:
+# ular qancha vaqtdan keyin uchrashadi?
+# qancha vaqtdan keyin ular orasidagi masofa 24km ni tashkil etadi?
+
+v1 = 4 #km/soat
+v2 = 2 #km/soat
+s = 24 #km
+v = v1 + v2
+t = s / v
+s = v * t
+t = s / v
+print('t=', t , 'soat')
+
+s1 = 24 - 12 
+s1 = 12
+t1 = s1 / v
+print('t1 = ', t1 , 'soat')
