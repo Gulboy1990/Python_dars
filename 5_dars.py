083f8066bea63ed29d35841e47b5fb0f767ec42c
Gulboy <76957086+Gulboy1990@users.noreply.github.com>
2021-01-12 17:38:52 +0500

    Add files via upload

---
+++ 5_dars.py
@@ -0,0 +1,15 @@
+
+# Quydagi o'zgaruvchilar turini aniqlang:
+
+# alfa = 8764;     beta = 'Lola';      Son = 30.2;
+
+alfa = 8764
+print(type(alfa))
+
+
+beta = 'Lola'
+print(type(beta))
+
+
+son = 30.2
+print(type(son))
---
+++ 6_dars.py
@@ -0,0 +1,6 @@
+
+# O'zgaruvchi uchun qiymat kiritilganda. uning turini 
+# aniqlovchi dastur tuzing:
+
+a = input('a =')
+print(type( a ))
