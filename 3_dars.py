1a1c780f3dd548a5016bc6443937ff5d2385c060
Gulboy <76957086+Gulboy1990@users.noreply.github.com>
2021-01-12 16:53:49 +0500

    Add files via upload

---
+++ 3_dars.py
@@ -0,0 +1,12 @@
+
+# Doiraninmg radiusi 4 metr teng (pi=3.14)"
+# doira yuzini hisoblang:
+# aylana uzunligini hisoblang.
+
+pi = 3.14
+r = 4 #metr
+s = pi * r *r
+print('s=' , s ,'metr kvadrat')
+
+i = 2 * pi * r
+print('i=' , i , 'metr')
